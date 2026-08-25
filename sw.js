a0e61f837bbf4229c615589dcc8768c251f79acb
ikhsanbachrul09 <ikhsanbachrul09@gmail.com>
2026-08-25 11:30:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // walau sedang tidak ada koneksi. Data transaksi TETAP butuh koneksi
 // internet karena selalu diambil langsung dari Google Apps Script.
 
-const CACHE_NAME = 'kas-pribadi-shell-v18';
+const CACHE_NAME = 'kas-pribadi-shell-v19';
 const SHELL_FILES = [
   './',
   './index.html',
